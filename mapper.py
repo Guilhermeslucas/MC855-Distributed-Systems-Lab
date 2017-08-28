3948427b379d61c7f76a2fb67eaa734613ba824c
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-08-28 16:16:08 -0300

    trying with exception

--- mapper.py
+++ mapper.py
@@ -1,17 +1,24 @@
 import urllib.request as ur
-from time import sleep
 from bs4 import BeautifulSoup
 
 f = open('github_links.txt','r')
 
+def try_connection(link):
+    from time import sleep
+    try:
+        return ur.urlopen(link).read()
+    except Exception as e:
+        print(e)
+        sleep(5)
+        return try_connection(link)
+
 for link in f.read().split('\n'):
     if link == '':
         break
-    response = ur.urlopen(link).read()
+    response = try_connection(link)
     soup = BeautifulSoup(response,'html.parser')     
     for links in soup.find_all('div', {'class':'repo-list-item'}):
         a = links.find('a')
         print(a['href'])
-    sleep(5)
 
 f.close()
