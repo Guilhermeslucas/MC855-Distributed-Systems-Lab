4178431d0ca6600ff71aad1bd02a8d72ab5d4583
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-09-21 21:10:54 -0300

    adding spark experiment

---
+++ Spark_Experiment/get_data.py
@@ -0,0 +1,6 @@
+#!/bin/env python3
+
+url_train = 'https://www.dropbox.com/s/21v6bnz0lkcv2us/year-prediction-msd-train.txt.zip?dl=1'
+url_test = 'https://www.dropbox.com/s/h1tavv5fwvi56nw/year-prediction-msd-test.txt.zip?dl=1'
+
+
