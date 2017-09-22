1bfaeef1f41e54990699521602d40b0b714fe5e1
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-09-22 13:23:56 -0300

    better catter script

--- Spark_Experiment/catter.sh
+++ Spark_Experiment/catter.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-output="$(~/Documentos/Unicamp/MC855/hadoop-2.7.4/bin/hdfs dfs -cat /data/year-prediction-msd-test.txt)"
+if [ $1 = "test" ] 
+then
+    output="$(~/Documentos/Unicamp/MC855/hadoop-2.7.4/bin/hdfs dfs -cat /data/year-prediction-msd-test.txt)"
+elif [ $1 = "train" ]
+then
+    output="$(~/Documentos/Unicamp/MC855/hadoop-2.7.4/bin/hdfs dfs -cat /data/year-prediction-msd-train.txt)"
+else
+    echo "Not a valid option. Run ./catter.sh test or /catter.sh train"
+    exit
+fi
 
 for line in $output
 do
