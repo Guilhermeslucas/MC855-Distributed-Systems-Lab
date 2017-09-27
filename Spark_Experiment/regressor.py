909fe95f9497e33f8a4d8cb9cebf2af5d1ad396e
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-09-26 21:24:38 -0300

    going on

--- Spark_Experiment/regressor.py
+++ Spark_Experiment/regressor.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 import subprocess
 import pyspark
+
+train_input = subprocess.Popen(['./catter.sh', 'train'], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+
+for line in train_input.split('\n'):
+    print(line)
